8bd763fef733835dbd7656992eea62af92de0ffa
ylliucoder <ylliucoder@outlook.com>
2023-11-09 06:42:50 +0800

    mes

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -9,9 +9,7 @@ const buildPrettierCommand = (filenames) =>
 const eslintExtensions = ["js", "jsx", "ts", "tsx"].join(",");
 const prettierExtensions = (await prettier.getSupportInfo()).languages
   .map((language) => language.extensions)
-  .flatMap((extensions) => {
-    return extensions.map((extension) => extension.substring(1));
-  })
+  .flatMap((extensions) => extensions.map((extension) => extension.substring(1)))
   .join(",");
 
 export default {
