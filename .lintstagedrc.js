2277bc9df57a01efb06ed31e76268736b59744e1
ylliucoder <ylliucoder@outlook.com>
2023-11-09 06:25:20 +0800

    husky

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,23 +1,20 @@
-import { getSupportInfo } from "prettier";
+import prettier from "prettier";
 import path from "path";
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`;
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`;
 const buildPrettierCommand = (filenames) =>
-  `prettier --write ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" ")}`;
+  `prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(" ")}`;
 
 const eslintExtensions = ["js", "jsx", "ts", "tsx"].join(",");
-const prettierExtensions = (await getSupportInfo()).languages
+const prettierExtensions = (await prettier.getSupportInfo()).languages
   .map((language) => language.extensions)
   .flatMap((extensions) => {
     return extensions.map((extension) => extension.substring(1));
   })
   .join(",");
+
 export default {
   [`*.{${eslintExtensions}}`]: [buildEslintCommand],
-  [`*.{${prettierExtensions}`]: [buildPrettierCommand]
+  [`*.{${prettierExtensions}}`]: [buildPrettierCommand]
 };
