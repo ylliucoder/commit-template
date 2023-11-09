126603fea1b22a3cc40188cbece7e7db69b5853c
ylliucoder <ylliucoder@outlook.com>
2023-11-10 02:13:20 +0800

    :sparkles: feat: new feature

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   branches: ["master", { name: "next", channel: "next", prerelease: "rc" }, { name: "beta", prerelease: true }, { name: "alpha", prerelease: true }],
   extends: ["semantic-release-config-gitmoji"]
 };
